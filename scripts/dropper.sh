48637753dead3856687c5a5a9de43ec7e959df41
InfectedPacket <infectedpacket@gmail.com>
2017-06-22 20:19:03 -0700

    Added dropper script
    Added dropper script to download random firmware from a file containing
    URLs.

---
+++ scripts/dropper.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+N=20
+SRC_FILE=""
+DST_DIR=""
+PREFIX=""
+
+##### Functions
+function usage
+{
+    echo "usage: $0 [[-s|--src] <file> [-c|--count] <count> [-d|--dest] <directory>]"
+}
+
+clear
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --src  )           shift
+                                SRC_FILE=$1
+                                ;;
+        -d | --dest )    		shift
+								DST_DIR=$1
+                                ;;
+        -c | --count)    		shift
+								N=$1
+                                ;;	
+        -p | --prefix)    		shift
+								PREFIX=$1
+                                ;;									
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+mkdir -p $DST_DIR
+
+if [ ! -e SRC_FILE ]; then
+	echo "[-] Could not find file: $SRC_FILE"
+	exit 1
+fi
+
+if [ $N -lt 0 ]; then
+	echo "[-] Invalid sample size: $N"
+	exit 1
+fi
+
+echo "[=] Downloading $N file(s) from $SRC_FILE..."
+for FILE in `shuf -n $N $SRC_FILE`; do
+	echo "[>]\t$FILE"
+	wget --tries=45 --mirror -o dropper.log -e robots=off -A zip,pkg,rar,bin,img,gz,7z --ignore-case $FILE -P $DST_DIR &> /dev/null
+done
+
+echo "[+] Completed. Terminating..."
+exit 0
